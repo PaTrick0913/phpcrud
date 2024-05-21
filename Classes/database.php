d87ae536cb29668dfbcd693297e09ce7aba56b26
PaTrick0913 <ptrkrmseva@gmail.com>
2024-05-21 17:51:12 +0800

    first commit

---
+++ Classes/database.php
@@ -0,0 +1,26 @@
+<?php
+
+class database{
+    function opencon(){
+        return new PDO ('mysql:host=localhost; dname=phpcrud', 'root','');
+    }
+
+    function signupUser($firstname, $lastname, $birthday, $sex, $email, $username, $password, $profile_picture_path){
+
+        
+            $con = $this->opencon();
+            // Save user data along with profile picture path to the database
+            $con->prepare("INSERT INTO users (user_firstname, user_lastname, user_birthday, user_sex, user_email, user_name, user_pass, user_profile_picture) VALUES (?,?,?,?,?,?,?,?)")->execute([$firstname, $lastname, $birthday, $sex, $email, $username, $password, $profilePicture]);
+            return $con->lastInsertId();
+            }
+        
+    
+        
+        function insertAddress($user_id, $street, $barangay, $city, $province)
+        {
+            $con = $this->opencon();
+            return $con->prepare("INSERT INTO user_address (user_id, street, barangay, city, province) VALUES (?,?,?,?,?)")->execute([$user_id, $street, $barangay,  $city, $province]);
+              
+        }
+    
+    }
